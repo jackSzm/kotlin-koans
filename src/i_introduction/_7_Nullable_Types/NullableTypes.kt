9a80b98dd3222f74b64811501ff7aacd2e3c5cd3
jackSzm <jszmelter@gmail.com>
2017-05-31 17:41:08 +0100

    Solve lesson 1.7

--- src/i_introduction/_7_Nullable_Types/NullableTypes.kt
+++ src/i_introduction/_7_Nullable_Types/NullableTypes.kt
@@ -23,11 +23,9 @@ fun todoTask7(client: Client?, message: String?, mailer: Mailer): Nothing = TODO
 )
 
 fun sendMessageToClient(client: Client?, message: String?, mailer: Mailer) {
-    if (message == null && client.personalInfo?.email == null) {
-        return
-    } else
-        mailer.sendMessage(client.personalInfo.email!!, message)
-}
+    val email = client?.personalInfo?.email
+    if (message != null && email != null)
+        mailer.sendMessage(email, message)
 }
 
 class Client(val personalInfo: PersonalInfo?)
